d77c7a693aa7100b8a8c64f1b0e6172864df63e9
night <>
2023-10-09 13:11:58 +0300

    fix: scroll logic updated

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,32 @@
-use leptos::html::{Body, Section};
+use js_sys::Math::log;
+use leptos::html::{AnyElement, Body, ElementDescriptor, Section, ToHtmlElement};
+use leptos::leptos_dom::HydrationKey;
 use leptos::logging::log;
 use leptos::{html::Nav, *};
 use leptos_meta::*;
 use leptos_router::*;
 use leptos_use::use_window_scroll;
+use std::ops::Deref;
+use wasm_bindgen::JsValue;
 
-#[derive(Clone, PartialEq, Debug)]
-enum ScrollSection {
-    None,
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum ScrollSection {
+    HomeTop,
     Home,
     About,
+    GalleryTop,
+    Gallery,
 }
 
 #[derive(Copy, Clone, Debug)]
 struct GlobalState {
-    home_section: RwSignal<NodeRef<Section>>,
-    about_section: RwSignal<NodeRef<Section>>,
+    section: RwSignal<ScrollSection>,
 }
 
 impl GlobalState {
     pub fn new() -> Self {
         Self {
-            home_section: create_rw_signal(create_node_ref::<html::Section>()),
-            about_section: create_rw_signal(create_node_ref::<html::Section>()),
+            section: create_rw_signal(ScrollSection::HomeTop),
         }
     }
 }
@@ -36,10 +40,11 @@ pub fn App() -> impl IntoView {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
         <Title text="Welcome to Leptos"/>
-        <Body class="text-low-purple bg-gradient-to-br from-mid-purple to-dark-purple" />
+        <Body class="text-low-purple pt-4 bg-gradient-to-br from-mid-purple to-dark-purple" />
+
         <Router>
             <Navbar/>
-            <main id="home" on:scroll=|_|{ logging::log!("SCROLLED!"); }  class=" grid grid-rows-[auto_1fr] pt-6 gap-6       ">
+            <main id="home" on:scroll=|_|{ logging::log!("SCROLLED!"); }  class=" grid grid-rows-[auto_1fr] pt-4 gap-6       ">
                 <Routes>
                     <Route path="" view=HomePage/>
                     <Route path="/gallery" view=GalleryPage/>
@@ -52,82 +57,168 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn Navbar() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let (scroll_section, set_scroll_section) = create_signal(ScrollSection::None);
-
     let global_state = use_context::<GlobalState>().expect("Failed to provide global state");
-    let home_section = global_state.home_section.get();
-    let about_section = global_state.about_section.get();
-
-    let navigate = leptos_router::use_navigate();
-
-    create_effect(move |_| {
-        let (x, y) = use_window_scroll();
-        let y = y();
-        let current_section = scroll_section();
-
-        let home_section_y: i32 = get_offset(home_section) - 70;
-        let about_section_y: i32 = get_offset(about_section) - 70;
-
-        let new_section = match y {
-            n if n > about_section_y as f64 => ScrollSection::About,
-            n if n > home_section_y as f64 => ScrollSection::Home,
-            _ => ScrollSection::None,
-        };
-
-        log!("{}", y);
-
-        if new_section != current_section {
-            set_scroll_section(new_section);
-        }
-    });
-
-    let btn_click = move |_| {
-        log!("wowowow");
-    };
+    let section = global_state.section;
 
     view! {
-        <nav  id="thenav" class=move || { format!("sticky text-low-purple top-0 z-50 px-6 flex items-center justify-between gap-2  {}", if scroll_section() != ScrollSection::None { "bg-gradient-to-r from-mid-purple to-dark-purple" } else { "" } ) }>
+        <nav  id="thenav" class=move || { format!("sticky  text-low-purple top-0 z-50 px-6 flex items-center justify-between gap-2 transition-all duration-500    {}", if section() == ScrollSection::HomeTop || section() == ScrollSection::GalleryTop { "bg-transparent"  } else { "bg-gradient-to-r from-mid-purple to-dark-purple" } ) }>
             <div class="flex items-center gap-6">
-                <h3 class="  font-bold text-[2rem] ">"ArtCord"</h3>
+                <a href="/" class="  font-bold text-[2rem] ">{  move || format!("ArtCord {:?}", section()) }</a>
                 <ul class="hidden sm:flex gap-2 text-[1rem] text-center">
-                    <li><a class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if scroll_section() == ScrollSection::None || scroll_section() == ScrollSection::Home { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } href="#home">"Home"</a></li>
-                    <li><a class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if scroll_section() == ScrollSection::About { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } href="#about">"About"</a></li>
-                    <li><a class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if false { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } href="/gallery">"Gallery"</a></li>
+                    <li><a href="/#home" class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if section() == ScrollSection::HomeTop || section() == ScrollSection::Home  { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } >"Home"</a></li>
+                    <li><a href="/#about" class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if section() == ScrollSection::About { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } >"About"</a></li>
+                    <li><a href="/gallery" class=move || { format!( " w-[3.5rem] cursor-pointer border-b-[0.30rem] transition duration-300 font-bold {} ", if section() == ScrollSection::Gallery || section() == ScrollSection::GalleryTop { "border-low-purple font-bold" } else { "border-transparent hover:border-low-purple/40 text-low-purple/60 hover:text-low-purple " } ) } >"Gallery"</a></li>
                 </ul>
             </div>
-            <button on:click=btn_click>
-                <div class="hidden sm:flex gap-2 items-center text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " href="">
+            <a target="_blank" href="https://discord.gg/habmw7Ehga">
+                <div class="hidden  sm:flex gap-2 items-center text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " >
                     <img src="/assets/discord.svg"/>
                     "Join"
                 </div>
-                <img class="cursor-pointer block sm:hidden " src="assets/burger.svg" alt=""/>
-            </button>
+                <img class="  cursor-pointer block sm:hidden " src="assets/burger.svg" alt=""/>
+            </a>
         </nav>
     }
 }
 
-fn get_offset(element: NodeRef<Section>) -> i32 {
+fn get_window_path() -> String {
+    let location = window().location();
+    let path = location.pathname();
+    let hash = location.hash();
+    if let (Ok(path), Ok(hash)) = (path, hash) {
+        format!("{}{}", path, hash)
+    } else {
+        String::from("/")
+    }
+}
+
+pub trait OffsetTop {
+    fn y(&self) -> i32;
+}
+
+impl OffsetTop for leptos::HtmlElement<Section> {
+    fn y(&self) -> i32 {
+        self.offset_top()
+    }
+}
+
+fn get_element_y<T: ElementDescriptor + Clone>(element: NodeRef<T>) -> i32
+where
+    leptos::HtmlElement<T>: OffsetTop,
+{
+    let test = element.get();
+    let a = test.unwrap().y();
+
     let mut section_y: i32 = 0;
-    let home_section = element.get();
-    if let Some(section) = home_section {
-        section_y = section.offset_top();
+    if let Some(section) = element.get() {
+        let a = section.y();
+        section_y = a;
     }
     section_y
 }
 
+fn silent_navigate(state: &str, unused: &str, url: &str) {
+    let a = window().history();
+    if let Ok(a) = a {
+        a.push_state_with_url(&JsValue::from(state), unused, Some(url))
+            .unwrap();
+    }
+}
+
 #[component]
 fn GalleryPage() -> impl IntoView {
+    let global_state = use_context::<GlobalState>().expect("Failed to provide global state");
+    let section = global_state.section;
+
+    let gallery_section = create_node_ref::<Section>();
+    let scroll_items = [ScrollDetect::new(
+        ScrollSection::Gallery,
+        gallery_section,
+        0,
+        "/gallery",
+    )];
+
+    create_effect(move |_| {
+        ScrollDetect::calc_section(section, ScrollSection::GalleryTop, &scroll_items);
+        // if section.get() != ScrollSection::Gallery {
+        //     section.set(ScrollSection::Gallery);
+        // }
+    });
+
     view! {
-        <h1>GALLERY</h1>
+        <section _ref=gallery_section style=move|| format!("min-height: calc(100vh - 100px)")>
+            <h1>GALLERY</h1>
+        </section>
+    }
+}
+
+pub struct ScrollDetect<'a, T: ElementDescriptor + 'static> {
+    pub node_ref: NodeRef<T>,
+    pub offset: i32,
+    pub path: &'a str,
+    pub id: ScrollSection,
+}
+
+impl<'a, T: ElementDescriptor> ScrollDetect<'a, T> {
+    pub fn new(id: ScrollSection, node_ref: NodeRef<T>, offset: i32, path: &'a str) -> Self {
+        Self {
+            node_ref,
+            offset,
+            path,
+            id,
+        }
+    }
+}
+
+impl<'a, T: ElementDescriptor + Clone> ScrollDetect<'a, T> {
+    pub fn calc_section(
+        section: RwSignal<ScrollSection>,
+        default: ScrollSection,
+        scroll_items: &[ScrollDetect<'a, T>],
+    ) -> ()
+    where
+        leptos::HtmlElement<T>: OffsetTop,
+    {
+        let current_section: ScrollSection = section.get();
+
+        for scroll_item in scroll_items {
+            let (x, y) = use_window_scroll();
+            let element_y = get_element_y(scroll_item.node_ref) - scroll_item.offset;
+            //log!("{:?} : {} <= {}", scroll_item.id, element_y, y());
+            if element_y as f64 <= y() {
+                //log!("{:?} == {:?}", scroll_item.id, current_section);
+                if scroll_item.id != current_section {
+                    //log!("SET FROM {:?} TO {:?}", current_section, scroll_item.id);
+                    section.set(scroll_item.id);
+                    silent_navigate(scroll_item.path, "", scroll_item.path);
+                }
+                return ();
+            }
+        }
+
+        if current_section != default {
+            //log!("BOOM {:?} == {:?}", ScrollSection::None, current_section);
+            section.set(default);
+        }
     }
 }
 
 #[component]
 fn HomePage() -> impl IntoView {
     let global_state = use_context::<GlobalState>().expect("Failed to provide global state");
-    let home_section = global_state.home_section.get();
-    let about_section = global_state.about_section.get();
+    let section = global_state.section;
+
+    let home_section = create_node_ref::<Section>();
+    let about_section = create_node_ref::<Section>();
+
+    let scroll_items = [
+        ScrollDetect::new(ScrollSection::About, about_section, 70, "/#about"),
+        ScrollDetect::new(ScrollSection::Home, home_section, 70, "/#home"),
+    ];
+
+    create_effect(move |_| {
+        ScrollDetect::calc_section(section, ScrollSection::HomeTop, &scroll_items);
+    });
 
     view! {
         <section _ref=home_section class="px-6 py-6 line-bg grid grid-rows-[1fr_1fr_0.3fr] md:grid-rows-[1fr] md:grid-cols-[1fr_1fr] place-items-center  overflow-hidden " style=move|| format!("min-height: calc(100vh - 100px)")>
@@ -137,7 +228,7 @@ fn HomePage() -> impl IntoView {
                         <h2 class="text-[2rem]">"Discord Art Server"</h2>
                         <div class="flex gap-8 mt-4 items-center justify-center">
                             <a class=" text-[1rem] cursor-pointer border-b-[0.30rem] border-low-purple font-bold whitespace-nowrap">"Read More"</a>
-                            <a class="flex gap-2 items-center text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " href="">
+                            <a target="_blank" href="https://discord.gg/habmw7Ehga" class="flex gap-2 items-center text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " >
                                 <img src="/assets/discord.svg"/>
                                 "Join"
                             </a>
@@ -151,7 +242,7 @@ fn HomePage() -> impl IntoView {
                         <div class="z-10 w-[32vw] h-[55vw] lg:max-w-[15rem] lg:max-h-[25rem] max-w-[10rem] max-h-[20rem] bg-center bg-cover absolute -rotate-[15deg] -translate-x-[60%]" style="background-image: url('/assets/3.jpg')" ></div>
                     </div>
                     <div class="flex justify-center">
-                        <a class=" shadow-glowy text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " href="">
+                        <a class=" shadow-glowy text-[1rem] font-black bg-half-purple border-[0.30rem] border-low-purple rounded-3xl px-4 py-[0.15rem] hover:bg-dark-purple transition-colors duration-300 " >
                             "View Gallery"
                         </a>
                     </div>
