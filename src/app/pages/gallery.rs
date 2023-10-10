43f49d479539a0acbad66d483146b5420844a25d
night <>
2023-10-10 15:29:15 +0300

    feature!: borked gallery render

--- src/app/pages/gallery.rs
+++ src/app/pages/gallery.rs
@@ -9,11 +9,86 @@ use std::rc::Rc;
 //use leptos_use::use_resize_observer;
 use rand::prelude::*;
 
-fn render_gallery(width: i32, images: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+fn render_gallery(max_width: i32, images: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    //log!("width: {}", max_width);
+
+    let max_width = max_width - 48;
+
     let mut resized_images: Vec<(i32, i32)> = Vec::new();
 
-    for image in images {
-        resized_images.push(image.to_owned());
+    //let current_row_images: &[(i32, i32)] = &[];
+    let mut new_row_start = 0;
+    let mut new_row_end = 0;
+    let mut current_row_filled_width: i32 = 0;
+
+    for (index, (w, h)) in images.iter().enumerate() {
+        // let new_width: i32 = w.to_owned();
+        // let new_height: i32 = h.to_owned();
+        let new_height: i32 = 250;
+        let width: i32 = w.to_owned();
+        let height: i32 = h.to_owned();
+        let ratio: f32 = width as f32 / height as f32;
+        let height_diff: i32 = height - new_height;
+        let new_width: i32 = width - (height_diff as f32 * ratio) as i32;
+
+        if (current_row_filled_width + new_width) <= max_width {
+            current_row_filled_width += new_width;
+            new_row_end = index;
+        } else {
+            let filled_with_diff = max_width - current_row_filled_width;
+            let img_count: usize = (new_row_end - new_row_start) + 1;
+            //log!("{}", img_count);
+
+            if img_count != 0 {
+                let add_width: f32 = filled_with_diff as f32 / img_count as f32;
+                log!(
+                    "i: {}, f: {}, w: {}, c: {}, d: {}",
+                    index,
+                    current_row_filled_width,
+                    max_width,
+                    img_count,
+                    add_width
+                );
+
+                for i in new_row_start..(new_row_end + 1) {
+                    let (prev_img_w, prev_img_h) = resized_images[i];
+                    let ratio = prev_img_w as f32 / prev_img_h as f32;
+                    let new_prev_img_w: f32 = max_width as f32 / img_count as f32;
+                    let new_prev_img_h: f32 = new_prev_img_w / ratio;
+                    //resized_images[i].0 = new_prev_img_w as i32;
+
+                    resized_images[i].0 += add_width as i32;
+                    //resized_images[i].0 = new_prev_img_w as i32 + add_width as i32;
+                    //resized_images[i].1 = new_prev_img_h as i32;
+                }
+            } else {
+                log!(
+                    "i: {}, f: {}, w: {}, c: {}, d: {}",
+                    index,
+                    current_row_filled_width,
+                    max_width,
+                    img_count,
+                    0
+                );
+            }
+
+            // if filled_with_diff != 0 {
+            //     let img_count: usize = new_row_end - new_row_start;
+            //     let add_width: f32 = filled_with_diff as f32 / img_count as f32;
+            //     for i in new_row_start..(new_row_end + 1) {
+            //         let (prev_img_h, prev_img_w) = resized_images[i];
+            //         let ratio = prev_img_w as f32 / prev_img_h as f32;
+            //         let add_height: i32 = (add_width / ratio) as i32;
+            //         resized_images[i].0 += add_height;
+            //         resized_images[i].1 += add_width as i32;
+            //     }
+            // }
+            new_row_start = index;
+            new_row_end = index;
+            current_row_filled_width = new_width;
+        }
+
+        resized_images.push((new_width, new_height));
     }
 
     resized_images
@@ -76,9 +151,9 @@ pub fn GalleryPage() -> impl IntoView {
                 set_gallery_width(width);
 
                 let imgs = imgs.clone();
-                let img = render_gallery(gallery_width(), &imgs);
+                let img = render_gallery(gallery_width.get_untracked(), &imgs);
                 set_gallery_images(img);
-                log!("width: {}", width);
+                //log!("width: {}", width);
             };
         });
 
@@ -88,9 +163,9 @@ pub fn GalleryPage() -> impl IntoView {
             set_gallery_width(width);
 
             let imgs = images.clone();
-            let img = render_gallery(gallery_width(), &imgs);
+            let img = render_gallery(gallery_width.get_untracked(), &imgs);
             set_gallery_images(img);
-            log!("INITIAL: {}", width);
+            //log!("INITIAL: {}", width);
         };
         //log!("SOMETHING WENT WRONG");
         //log!("first load");
@@ -144,20 +219,21 @@ pub fn GalleryPage() -> impl IntoView {
     });
 
     view! {
-        <section on:resize=move |_| { log!("test resize") } _ref=gallery_section class="line-bg  px-6 flex gap-2 flex-wrap  " style=move|| format!("min-height: calc(100vh - 100px)")>
+        <section on:resize=move |_| { log!("test resize") } _ref=gallery_section class="line-bg  px-6 flex flex-wrap  " style=move|| format!("min-height: calc(100vh - 100px)")>
             { move || {
 
-                  gallery_images.get().into_iter().map(|(h, w)|{
+                  gallery_images.get().into_iter().map(|(w, h)|{
 
                     view! {
                         <div
-                            class="flex-shrink-0 flex flex-col shadow-glowy  bg-mid-purple border-4 border-low-purple"
+                            class="flex-shrink-0 font-bold grid place-items-center shadow-glowy  bg-mid-purple border-4 border-low-purple"
                             style:height=move || format!("{}px", h)
                             style:width=move || format!("{}px", w)
                         >
-                            <div class="flex justify-between gap-2">
-                                <h3>hello</h3>
-                                <div>2020</div>
+                            <div class="flex flex-col text-center justify-center gap-2">
+                                <h3>{w}x{h}</h3>
+                                <h3>{w as f32 /h as f32}</h3>
+                                // <div>2020</div>
                             </div>
                         </div>
                     } }).collect_view()
