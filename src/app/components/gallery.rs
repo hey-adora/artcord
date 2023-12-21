981712a864ed7aaf0e60a51ef702ce0ef8dc2f29
hey <>
2023-12-21 16:00:06 +0200

    fix: gallery component

--- src/app/components/gallery.rs
+++ src/app/components/gallery.rs
@@ -2,7 +2,7 @@ use crate::app::components::navbar::{shrink_nav, Navbar};
 use bson::DateTime;
 use chrono::Utc;
 use leptos::ev::resize;
-use leptos::html::Section;
+use leptos::html::{ElementDescriptor, Section};
 use leptos::logging::log;
 use leptos::*;
 use leptos_use::{use_event_listener, use_window};
@@ -13,15 +13,6 @@ use crate::app::utils::{GlobalState, ServerMsgImgResized};
 use crate::server::{ClientMsg, ServerMsg, SERVER_MSG_IMGS_NAME};
 
 pub const NEW_IMG_HEIGHT: u32 = 250;
-
-#[derive(Clone)]
-struct SelectedImg {
-    pub org_url: String,
-    pub author_name: String,
-    pub author_pfp: String,
-    pub width: u32,
-    pub height: u32,
-}
 
 pub trait GalleryImg {
     fn get_size(&self) -> (u32, u32);
@@ -77,7 +68,6 @@ pub fn resize_imgs<T: GalleryImg>(new_height: u32, max_width: u32, imgs: &mut [T
     let mut top: f32 = 0.0;
 
     let mut rand = rand::thread_rng();
-
     for index in loop_start..loop_end {
         let org_img = &mut imgs[index];
         let (width, height) = org_img.get_size();
@@ -124,14 +114,18 @@ fn create_client_test_imgs() -> Vec<ServerMsgImgResized> {
     new_imgs
 }
 
+//F: Fn(ServerMsgImgResized) -> IV + 'static, IV: IntoView
 #[component]
-pub fn Gallery<F: Fn() -> IV, IV: IntoView>(
+pub fn Gallery<
+    OnClick: Fn(ServerMsgImgResized) + Copy + 'static,
+    OnFetch: Fn(DateTime, u32) + Copy + 'static,
+>(
     global_gallery_imgs: RwSignal<Vec<ServerMsgImgResized>>,
-    render_prop: F,
+    on_click: OnClick,
+    on_fetch: OnFetch,
 ) -> impl IntoView {
     let global_state = use_context::<GlobalState>().expect("Failed to provide global state");
     let gallery_section = create_node_ref::<Section>();
-    let selected_img: RwSignal<Option<SelectedImg>> = create_rw_signal(None);
     let nav_tran = global_state.nav_tran;
 
     // create_effect(move |_| {
@@ -145,6 +139,46 @@ pub fn Gallery<F: Fn() -> IV, IV: IntoView>(
     //         };
     //     });
     // });
+    let section_scroll = move |_: Event| {
+        if !global_state.socket_state_is_ready(SERVER_MSG_IMGS_NAME) {
+            return;
+        }
+
+        let Some(last) = global_gallery_imgs.with_untracked(|imgs| match imgs.last() {
+            Some(l) => Some(l.created_at),
+            None => None,
+        }) else {
+            return;
+        };
+
+        let section = gallery_section;
+
+        let Some(section) = gallery_section.get_untracked() else {
+            return;
+        };
+
+        let scroll_top = section.scroll_top();
+        let client_height = section.client_height();
+        let scroll_height = section.scroll_height();
+        let client_width = section.client_width();
+
+        shrink_nav(nav_tran, scroll_top as u32);
+
+        let left = scroll_height - (client_height + scroll_top);
+
+        if left < client_height {
+            on_fetch(
+                last,
+                calc_fit_count(client_width as u32, client_height as u32) * 2,
+            );
+            // let msg = ClientMsg::GalleryInit {
+            //     amount: calc_fit_count(client_width as u32, client_height as u32) * 2,
+            //     from: last,
+            // };
+            // global_state.socket_send(msg);
+            global_state.socket_state_used(SERVER_MSG_IMGS_NAME);
+        }
+    };
 
     create_effect(move |_| {
         let _ = use_event_listener(use_window(), resize, move |_| {
@@ -177,94 +211,30 @@ pub fn Gallery<F: Fn() -> IV, IV: IntoView>(
         let client_height = section.client_height();
         let client_width = section.client_width();
 
-        let msg = ClientMsg::GalleryInit {
-            amount: calc_fit_count(client_width as u32, client_height as u32) * 2,
-            from: DateTime::from_millis(Utc::now().timestamp_millis()),
-        };
+        on_fetch(
+            DateTime::from_millis(Utc::now().timestamp_millis()),
+            calc_fit_count(client_width as u32, client_height as u32) * 2,
+        );
 
-        global_state.socket_send(msg);
+        // let msg = ClientMsg::GalleryInit {
+        //     amount: calc_fit_count(client_width as u32, client_height as u32) * 2,
+        //     from: DateTime::from_millis(Utc::now().timestamp_millis()),
+        // };
+
+        //global_state.socket_send(msg);
         global_state.gallery_loaded.set(true);
     });
 
-    let section_scroll = move |_: Event| {
-        if !global_state.socket_state_is_ready(SERVER_MSG_IMGS_NAME) {
-            return;
-        }
-
-        let Some(last) = global_gallery_imgs.with_untracked(|imgs| match imgs.last() {
-            Some(l) => Some(l.created_at),
-            None => None,
-        }) else {
-            return;
-        };
-
-        let Some(section) = gallery_section.get_untracked() else {
-            return;
-        };
-
-        let scroll_top = section.scroll_top();
-        let client_height = section.client_height();
-        let scroll_height = section.scroll_height();
-        let client_width = section.client_width();
-
-        shrink_nav(nav_tran, scroll_top as u32);
-
-        let left = scroll_height - (client_height + scroll_top);
-
-        if left < client_height {
-            let msg = ClientMsg::GalleryInit {
-                amount: calc_fit_count(client_width as u32, client_height as u32) * 2,
-                from: last,
-            };
-            global_state.socket_send(msg);
-            global_state.socket_state_used(SERVER_MSG_IMGS_NAME);
-        }
-    };
-
-    let select_click_img = move |img: &ServerMsgImgResized| {
-        selected_img.set(Some(SelectedImg {
-            org_url: img.display_high.clone(),
-            author_name: img.user.name.clone(),
-            author_pfp: format!("/assets/gallery/pfp_{}.webp", img.user.id.clone()),
-            width: img.width,
-            height: img.height,
-        }))
-    };
     view! {
-        {
-            move || {
-                match selected_img.get() {
-                    Some(img) => Some(view! {
-                        <div on:click=move |_| { selected_img.set(None); } class=" absolute grid grid-rows-[1fr] left-0 top-0 w-screen h-[100dvh] place-items-center bg-gradient-to-br from-mid-purple/50 to-dark-purple/50 z-[150] ">
-                            <div  >
-                                <div class="flex justify-between items-center rounded-t-lg bg-dark-purple pl-2">
-                                       <div class="flex gap-2">
-                                            <div>"By "</div>
-                                            <img class="border border-low-purple rounded-full bg-mid-purple h-[25px] " src=img.author_pfp/>
-                                            <div>{img.author_name}</div>
-                                       </div>
-                                     <img class="cursor-pointer border-2 border-low-purple rounded-full bg-mid-purple w-[30px] h-[30px] p-1 m-2" src="assets/x.svg"/>
-                                </div>
-                                <img class="bg-mid-purple object-contain " alt="loading..." style=move|| format!("max-height: calc(100dvh - 70px); max-width: 100vw; height: min({1}px, calc(100vw * ( {1} / {0} ))); aspect-ratio: {0} / {1};", img.width, img.height) on:click=move |e| { e.stop_propagation();  } src=img.org_url/>
-                            </div>
-                        </div> }),
-                None => None
-                }
-            }
-        }
-
         <section id="gallery_section" on:scroll=section_scroll _ref=gallery_section class="relative content-start overflow-x-hidden overflow-y-scroll h-full" >
-            <For each=global_gallery_imgs key=|state| state._id let:img >
-                {
-
-                        // let (w, h) = img.get_size();
-                        // let height = ;
-                        // let with = format!("{}px", w);
-                        let height = img.new_height;
-                        let width = img.new_width;
-                        let top = img.top;
-                        let left = img.left;
-                        let bg_img = format!("url('{}')", &img.display_preview);
+            <For each=move || global_gallery_imgs.get().into_iter().enumerate()  key=|state| state.1._id let:data > {
+                    let img = data.1;
+                    let i = data.0;
+                    let height = img.new_height;
+                    let width = img.new_width;
+                    let top = img.top;
+                    let left = img.left;
+                    let bg_img = format!("url('{}')", &img.display_preview);
 
                         view! {
                             <div
@@ -274,14 +244,12 @@ pub fn Gallery<F: Fn() -> IV, IV: IntoView>(
                                 style:top=move || format!("{}px", top.get())
                                 style:left=move || format!("{}px", left.get())
                                 style:background-image=bg_img
-                                on:click= move |_| select_click_img(&img)
+                                on:click=  move |_| on_click(global_gallery_imgs.with_untracked(|imgs|imgs[i].clone()))
                             >
-                                // <div class="relative flex opacity-0 hover:opacity-100 transition duration-300 w-full h-full flex-col text-center justify-center gap-2  "  >
-                                //     <div class="absolute bg-dark-purple bottom-0 left-0 translate-y-full w-full">{&img.user.name}</div>
-                                // </div>
                             </div>
                         }
-                }
+                    }
+
             </For>
         </section>
     }
