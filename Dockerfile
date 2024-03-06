4027bbfa8d951e18803f39ce23e163adcbdf660d
Your Name <you@example.com>
2024-03-06 17:18:28 +0200

    feature: full docker support

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM archlinux:latest
+
+RUN pacman -Sy
+RUN pacman --noconfirm -S rustup sudo git base-devel go
+RUN useradd -mG wheel crab
+RUN echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> "/etc/sudoers"
+USER crab
+WORKDIR /home/crab
+RUN git clone https://aur.archlinux.org/yay
+WORKDIR /home/crab/yay
+RUN makepkg --noconfirm -si PKGBUILD
+RUN yay --noconfirm -S nvm
+RUN bash -c "source /usr/share/nvm/init-nvm.sh && nvm install 21.6.1 && npm -g i tailwindcss"
+RUN rustup toolchain install stable
+RUN rustup target add wasm32-unknown-unknown
+RUN rustup default stable
+RUN cargo install cargo-leptos
--- artcord-leptos/src/lib.rs
+++ artcord-leptos/src/lib.rs
@@ -1,8 +1,18 @@
 pub mod app;
 
 use app::App;
-// use wasm_bindgen::prelude::wasm_bindgen;
-// use leptos::{logging::log, *};
+use wasm_bindgen::prelude::wasm_bindgen;
+use leptos::{logging::log, *};
+
+#[wasm_bindgen]
+pub fn hydrate() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    leptos::mount_to_body(App)
+}
+
+
 // use crate::leptos_dom::mount_to_with_stop_hydrating;
 
 // macro_rules! console_log {
@@ -14,13 +24,7 @@ use app::App;
 //     #[wasm_bindgen(js_namespace = console)]
 //     fn log(s: &str);
 // }
-// #[wasm_bindgen]
-// pub fn hydrate() {
-//     _ = console_log::init_with_level(log::Level::Debug);
-//     console_error_panic_hook::set_once();
 
-//     leptos::mount_to_body(App)
-// }
 // #[wasm_bindgen]
 // pub fn hydrate() {
 //     _ = console_log::init_with_level(log::Level::Debug);
@@ -40,18 +44,18 @@ use app::App;
 // }
 
 
-use cfg_if::cfg_if;
+// use cfg_if::cfg_if;
 
-cfg_if! {
-if #[cfg(feature = "hydrate")] {
+// cfg_if! {
+// if #[cfg(feature = "hydrate")] {
 
-    use wasm_bindgen::prelude::wasm_bindgen;
+//     use wasm_bindgen::prelude::wasm_bindgen;
 
-        #[wasm_bindgen]
-        pub fn hydrate() {
-        console_error_panic_hook::set_once();
+//         #[wasm_bindgen]
+//         pub fn hydrate() {
+//         console_error_panic_hook::set_once();
 
-        leptos::mount_to_body(App);
-        }
-    }
-}
+//         leptos::mount_to_body(App);
+//         }
+//     }
+// }
