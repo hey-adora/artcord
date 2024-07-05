7471e7c9a9aec964606fb428d2dcdaa3ec53b5c6
Your Name <you@example.com>
2024-07-05 17:33:21 +0300

    chore: more readable script

--- ssr_debug.sh
+++ ssr_debug.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-cargo build --package=artcord --no-default-features --features=ssr
-cargo build --package=artcord-leptos --target=wasm32-unknown-unknown --no-default-features --features=hydrate
-rm -rf ./target/site &&
-mkdir ./target/site &&
-mkdir ./target/site/pkg &&
+cargo build --package=artcord --no-default-features --features=ssr &&
+cargo build --package=artcord-leptos --target=wasm32-unknown-unknown --no-default-features --features=hydrate &&
+rm -rf ./target/site/* &&
+mkdir -p ./target/site/pkg &&
 cp -r ./assets/* ./target/site
 tailwindcss -i input.css -o output.css -c tailwind.config.js &&
 cp ./output.css ./target/site/pkg/leptos_start5.css &&
